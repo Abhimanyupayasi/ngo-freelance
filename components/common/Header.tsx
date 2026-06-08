dc61d78cf7f6bb0c3747564796d0328a03439b47
Abhimanyu <abhimanyupayasi01@gmail.com>
2026-06-08 21:55:15 +0530

    added : home page and header fixes

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -181,7 +181,7 @@ export default function Header() {
                         transition
                       "
                     >
-                      {item}
+                      {item.name}
                     </a>
                   ))}
                 </div>
